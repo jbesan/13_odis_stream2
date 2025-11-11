ff87e26f3b06492b79299942a12539789a65f62f
Jacques B. <jbesancon@gmail.com>
2025-11-11 15:23:56 +0100

    Optimisation Utilisation Mémoire

--- app/data_loader.py
+++ app/data_loader.py
@@ -59,13 +59,42 @@ def load_all_datasets(odis_file: str, scores_cat_file: str, metiers_file: str, f
     app_dir = os.path.dirname(os.path.abspath(__file__))
     config_path = os.path.join(app_dir, scores_cat_file)
 
-    odis = pd.read_parquet(base_path + odis_file)
+    # Define the specific columns to load from the main dataset to save memory
+    columns_to_load = [
+        'codgeo', 'libgeo', 'polygon', 'dep_code', 'epci_code', 'epci_nom', 'codgeo_voisins',
+        'population', 'pop_be', 'met', 'be_codfap_top',
+        'codes_formations', 'rp_5+pieces', 'log_rp', 'log_soc_inoccupes',
+        'log_soc_total', 'log_vac', 'log_total', 'risque_fermeture', 'ecoles_ct',
+        'svc_incl_count', 'pol_num'
+    ]
+    odis = pd.read_parquet(base_path + odis_file, columns=columns_to_load)
     odis['polygon'] = odis.polygon.apply(shp.from_wkb)
     odis = gpd.GeoDataFrame(odis, geometry='polygon', crs='EPSG:4326')
     odis.set_geometry('polygon', inplace=True)
     odis.polygon.set_precision(10**-5)
     odis = odis[~odis.polygon.isna()]
     odis.set_index('codgeo', inplace=True)
+
+    # --- Optimize Data Types ---
+    # Downcast integer columns to save memory
+    odis['population'] = odis['population'].astype('int32')
+    odis['met'] = odis['met'].astype(pd.Int32Dtype())
+    odis['ecoles_ct'] = odis['ecoles_ct'].astype(pd.Int16Dtype())
+    odis['risque_fermeture'] = odis['risque_fermeture'].astype(pd.Int16Dtype())
+    odis['svc_incl_count'] = odis['svc_incl_count'].astype(pd.Int16Dtype())
+
+    # Downcast float columns to save memory
+    float_cols = ['pop_be', 'rp_5+pieces', 'log_rp', 'log_soc_inoccupes',
+                  'log_soc_total', 'log_vac', 'log_total', 'pol_num']
+    for col in float_cols:
+        if col in odis.columns:
+            odis[col] = odis[col].astype('float32')
+
+    # Convert object columns to category to save memory
+    cat_cols = ['dep_code', 'epci_code']
+    for col in cat_cols:
+        if col in odis.columns:
+            odis[col] = odis[col].astype('category')
 
     # Index of all scores and their explanations.
     scores_cat = load_scores_config_as_df(config_path)
@@ -78,7 +107,14 @@ def load_all_datasets(odis_file: str, scores_cat_file: str, metiers_file: str, f
     codformations_index = pd.read_csv(base_path + formations_file, dtype={'codformation': str}).set_index('codformation')
 
     # Etablissements scolaires
-    annuaire_ecoles = pd.read_parquet(base_path + ecoles_file)
+    ecoles_cols_to_load = ['code_commune', 'nom_etablissement', 'type_etablissement', 'ecole_maternelle', 'ecole_elementaire', 'geometry']
+    annuaire_ecoles = pd.read_parquet(base_path + ecoles_file, columns=ecoles_cols_to_load)
+
+    # Optimize data types
+    annuaire_ecoles['type_etablissement'] = annuaire_ecoles['type_etablissement'].astype('category')
+    annuaire_ecoles['ecole_maternelle'] = annuaire_ecoles['ecole_maternelle'].astype(pd.Int8Dtype())
+    annuaire_ecoles['ecole_elementaire'] = annuaire_ecoles['ecole_elementaire'].astype(pd.Int8Dtype())
+
     annuaire_ecoles.geometry = annuaire_ecoles.geometry.apply(shp.from_wkb)
     annuaire_ecoles = gpd.GeoDataFrame(annuaire_ecoles, geometry='geometry', crs='EPSG:4326')
 
@@ -99,11 +135,17 @@ def load_all_datasets(odis_file: str, scores_cat_file: str, metiers_file: str, f
 
     # Annuaire des services d'inclusion
     # Pre-process inclusion data for faster lookup
-    annuaire_inclusion = pd.read_parquet(base_path + inclusion_file)
+    inclusion_cols_to_load = ['nom', 'codgeo', 'categorie', 'service', 'geometry']
+    annuaire_inclusion = pd.read_parquet(base_path + inclusion_file, columns=inclusion_cols_to_load)
+
+    # Optimize data types
+    annuaire_inclusion['categorie'] = annuaire_inclusion['categorie'].astype('category')
+    annuaire_inclusion['service'] = annuaire_inclusion['service'].astype('category')
+
     annuaire_inclusion.geometry = annuaire_inclusion.geometry.apply(shp.from_wkb)
     annuaire_inclusion = gpd.GeoDataFrame(annuaire_inclusion, geometry='geometry', crs='EPSG:4326')
     incl_index = annuaire_inclusion[['codgeo', 'categorie', 'service']].drop_duplicates()
-    incl_index['key'] = incl_index.categorie+'_'+incl_index.service
+    incl_index['key'] = incl_index.categorie.astype(str) + '_' + incl_index.service.astype(str)
     incl_index = incl_index.groupby('codgeo').agg({'key': lambda x: set(x)})
 
     return odis, scores_cat, codfap_index, codformations_index, annuaire_ecoles, annuaire_sante, annuaire_inclusion, incl_index
